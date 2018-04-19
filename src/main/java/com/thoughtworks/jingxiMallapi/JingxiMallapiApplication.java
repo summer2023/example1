41a477efdde85dbf4beebef3d4a81196148ef124
Lucerne <lucerne_liu@foxmail.com>
2018-04-19 15:30:19 +0800

    first commit

---
+++ src/main/java/com/thoughtworks/jingxiMallapi/JingxiMallapiApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.jingxiMallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingxiMallapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingxiMallapiApplication.class, args);
+	}
+}
